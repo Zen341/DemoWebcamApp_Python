ee97803f9f1939e1b333631e10520739f9957fdd
Zen341 <79434568+Zen341@users.noreply.github.com>
2023-01-16 08:54:56 +0700

    Test run #1

--- mainWindow.py
+++ mainWindow.py
@@ -2,6 +2,17 @@ from tkinter import *
 import cv2
 from PIL import Image, ImageTk
 import numpy as np
+from datetime import datetime as dt
+from pathlib import Path
+import os
+
+# Define a global Mat variable
+frame = np.zeros((rows, columns, channels), dtype = "uint8")
+
+# Define directory to save samples
+projDir = os.path.dirname(os.path.abspath(__file__))
+smplImgPath = projDir + "\\sample\\images\\"
+Path(smplImgPath).mkdir(parents=True, exist_ok=True)
 
 # Define a video capture object
 vid = cv2.VideoCapture(0)
@@ -25,7 +36,8 @@ label_widget = Label(app)
 label_widget.pack()
 
 isSharpen = IntVar()
-chbxImgSharp = Checkbutton(app, text='Image sharpening', variable=isSharpen).pack()
+chbxImgSharp = Checkbutton(app, text='Image sharpening', variable=isSharpen, onvalue=1, offvalue=0, command=lambda: print("check changed: " + str(isSharpen.get()))).pack()
+
 
 # Image sharpening
 def imgSharp(image):
@@ -41,6 +53,11 @@ def imgSharp(image):
 def open_camera():
     # Capture the video frame by frame
     _, frame = vid.read()
+
+    # Image enhance
+    # Sharpening
+    if isSharpen.get() == 1:
+        frame = imgSharp(frame)
 
     # Convert image from one color space to other
     opencv_image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
@@ -60,10 +77,27 @@ def open_camera():
     # Repeat the same process after every 10 seconds
     label_widget.after(10, open_camera)
 
+# The app will open camera automatically
+# # Create a button to open the camera in GUI app
+# button1 = Button(app, text="Open Camera", command=open_camera)
+# button1.pack()
 
-# Create a button to open the camera in GUI app
-button1 = Button(app, text="Open Camera", command=open_camera)
-button1.pack()
 
-# Create an infinite loop for displaying app on screen
+# Create a function to save image
+def save_image():
+    # _, frame = vid.read()
+    now = dt.now()
+    filename = smplImgPath + 'image_' + now.strftime("%Y_%m_%d_%H_%M_%S") + '.jpg'
+    cv2.imwrite(filename, frame)
+
+
+# Create a button to save image
+btnSave = Button(app, text="Capture", command=save_image)
+btnSave.pack()
+
+# Open camera after the app displayed for 1 second
+app.after(1000, open_camera())
+
+# Create an infinite loop for displaying app on screen (center screen)
+app.eval('tk::PlaceWindow . center')
 app.mainloop()
